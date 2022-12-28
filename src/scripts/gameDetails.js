4e4514f71cb3e5bed23943b077a747a38aa1789d
guilherme096 <guilherme.rosa60@gmail.com>
2022-12-28 18:16:32 +0000

    more things in games page

--- src/scripts/gameDetails.js
+++ src/scripts/gameDetails.js
@@ -1,10 +1,12 @@
-﻿// ViewModel KnockOut
+﻿import { flagCodes } from './flagCodes.js';
+// ViewModel KnockOut
 var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
     self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Games/FullDetails?id=');
-    self.statsUri = ko.observable('http://192.168.160.58/Olympics/api/Statistics/Games_Countries');
+    self.countrySumUri = ko.observable('http://192.168.160.58/Olympics/api/Statistics/Games_Countries');
+    self.countryMedalsUri = ko.observable('http://192.168.160.58/Olympics/api/Statistics/Medals_Country?id=');
     self.displayName = 'Olympic Games edition Details';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
@@ -21,24 +23,19 @@ var vm = function () {
     self.Modalities = ko.observableArray([]);
     self.Competitions = ko.observableArray([]);
     self.Medals = ko.observableArray([]);
+    self.GoldMedals = ko.observable();
+    self.SilverMedals = ko.observable();
+    self.BronzeMedals = ko.observable();
     self.Url = ko.observable('');
     self.countriesSum = ko.observable();
     self.AthletesSum = ko.observable();
+    self.topCountries = ko.observableArray([]);
 
     //--- Page Events
     self.activate = async function (id) {
         console.log('CALL: getGame...');
         var composedUri = self.baseUri() + id;
         await ajaxHelper(composedUri, 'GET').done(function (data) {
-            ajaxHelper(self.statsUri(), 'GET').done(function (data) {
-                var counter = 0;
-                for (var i = 0; i < data.length; i++) {
-                    if (data[i].Name == self.Name()) {
-                        counter = data[i].Counter;
-                    }
-                }
-                self.countriesSum(counter);
-            });
             console.log(data);
             self.Id(data.Id);
             self.CountryName(data.CountryName);
@@ -52,7 +49,44 @@ var vm = function () {
             self.Modalities(data.Modalities);
             self.Competitions(data.Competitions);
             self.Medals(data.Medals);
+            self.GoldMedals(self.Medals()[0].Counter);
+            self.SilverMedals(self.Medals()[1].Counter);
+            self.BronzeMedals(self.Medals()[2].Counter);
             self.AthletesSum(self.Athletes().length);
+        })
+        await ajaxHelper(self.countrySumUri(), 'GET').done(function (data) {
+            var counter = 0;
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].Name == self.Name()) {
+                    counter = data[i].Counter;
+                }
+            }
+            self.countriesSum(counter);
+        })
+        await ajaxHelper(self.countryMedalsUri() + self.Id(), 'GET').done(function (data) {
+            console.log(data);
+            var topCountries = [];
+            for (var i = 0; i < data.length; i++) {
+                var points = 0;
+                for (var j = 0; j < data[i].Medals.length; j++) {
+                    if (data[i].Medals[j].MedalName == "Gold") {
+                        points += data[i].Medals[j].Counter * 3;
+                    }
+                    else if (data[i].Medals[j].MedalName == "Silver") {
+                        points += data[i].Medals[j].Counter * 2;
+                    }
+                    else if (data[i].Medals[j].MedalName == "Bronze") {
+                        points += data[i].Medals[j].Counter * 1;
+                    }
+                }
+                topCountries.push({ Country: data[i].CountryName, Points: points, flagUrl: 'https://countryflagsapi.com/png/' + flagCodes[data[i].CountryName], GoldMedals: data[i].Medals[0].Counter, SilverMedals: data[i].Medals[1].Counter, BronzeMedals: data[i].Medals[2].Counter });
+            }
+            topCountries.sort(function (a, b) {
+                return b.Points - a.Points;
+            });
+            var top3 = topCountries.slice(0, 3);
+            self.topCountries(top3);
+            console.log(top3);
         });
         hideLoading();
     };
