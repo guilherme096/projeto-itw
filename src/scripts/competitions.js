a79f7daf27e0dbe25512cc7b40ba307e3faef25c
hujuc <hujucastro@gmail.com>
2023-01-02 09:27:44 +0000

    problemas do competitionDetails com sem fotografia

--- src/scripts/competitions.js
+++ src/scripts/competitions.js
@@ -1,13 +1,13 @@
 // ViewModel KnockOut
 var vm = function () {
-    console.log('ViewModel initiated...');
+    console.log("ViewModel initiated...");
     //---Variáveis locais
     var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/competitions');
+    self.baseUri = ko.observable("http://192.168.160.58/Olympics/api/competitions");
 
-    self.displayName = 'Olympic Competitions List';
-    self.error = ko.observable('');
-    self.passingMessage = ko.observable('');
+    self.displayName = "Olympic Competitions List";
+    self.error = ko.observable("");
+    self.passingMessage = ko.observable("");
     self.records = ko.observableArray([]);
     self.favourites = ko.observableArray([]);
     self.currentPage = ko.observable(1);
@@ -32,23 +32,18 @@ var vm = function () {
         var list = [];
         var size = Math.min(self.totalPages(), 9);
         var step;
-        if (size < 9 || self.currentPage() === 1)
-            step = 0;
-        else if (self.currentPage() >= self.totalPages() - 4)
-            step = self.totalPages() - 9;
-        else
-            step = Math.max(self.currentPage() - 5, 0);
+        if (size < 9 || self.currentPage() === 1) step = 0;
+        else if (self.currentPage() >= self.totalPages() - 4) step = self.totalPages() - 9;
+        else step = Math.max(self.currentPage() - 5, 0);
 
-        for (var i = 1; i <= size; i++)
-            list.push(i + step);
+        for (var i = 1; i <= size; i++) list.push(i + step);
         return list;
     };
 
     self.toggleFavourite = function (id) {
         if (self.favourites.indexOf(id) == -1) {
             self.favourites.push(id);
-        }
-        else {
+        } else {
             self.favourites.remove(id);
         }
         localStorage.setItem("fav2", JSON.stringify(self.favourites()));
@@ -57,37 +52,34 @@ var vm = function () {
         let storage;
         try {
             storage = JSON.parse(localStorage.getItem("fav2"));
-        }
-        catch (e) {
-            ;
-        }
+        } catch (e) {}
         if (Array.isArray(storage)) {
             self.favourites(storage);
         }
-    }
+    };
 
-    self.view = ko.observable(getUrlParameter('view') ? getUrlParameter('view') : 'card');
+    self.view = ko.observable(getUrlParameter("view") ? getUrlParameter("view") : "card");
 
     // Function to toggle the view
-    self.toggleTable = function() {
-      self.view('table');
+    self.toggleTable = function () {
+        self.view("table");
     };
-    self.toggleCard = function() {
-      self.view('card');
+    self.toggleCard = function () {
+        self.view("card");
     };
 
     //--- Page Events
     self.activate = function (id) {
-        console.log('CALL: getCompetitions...');
+        console.log("CALL: getCompetitions...");
         var composedUri = self.baseUri() + "?page=" + id + "&pageSize=" + self.pagesize();
-        ajaxHelper(composedUri, 'GET').done(function (data) {
+        ajaxHelper(composedUri, "GET").done(function (data) {
             console.log(data);
             hideLoading();
             self.records(data.Records);
             self.currentPage(data.CurrentPage);
             self.hasNext(data.HasNext);
             self.hasPrevious(data.HasPrevious);
-            self.pagesize(data.PageSize)
+            self.pagesize(data.PageSize);
             self.totalPages(data.TotalPages);
             self.totalRecords(data.TotalRecords);
             self.SetFavourites();
@@ -95,23 +87,23 @@ var vm = function () {
     };
 
     self.activate2 = function (search, page) {
-        console.log('CALL: searchCompetitions...');
+        console.log("CALL: searchCompetitions...");
         var composedUri = "http://192.168.160.58/Olympics/api/Competitions/SearchByName?q=" + search;
-        ajaxHelper(composedUri, 'GET').done(function (data) {
+        ajaxHelper(composedUri, "GET").done(function (data) {
             console.log("searchCompetitions", data);
             hideLoading();
             self.records(data.slice(0 + 21 * (page - 1), 21 * page));
             self.totalRecords(data.length);
             self.currentPage(page);
             if (page == 1) {
-                self.hasPrevious(false)
+                self.hasPrevious(false);
             } else {
-                self.hasPrevious(true)
+                self.hasPrevious(true);
             }
             if (self.records() - 21 > 0) {
-                self.hasNext(true)
+                self.hasNext(true);
             } else {
-                self.hasNext(false)
+                self.hasNext(false);
             }
             if (Math.floor(self.totalRecords() / 21) == 0) {
                 self.totalPages(1);
@@ -119,25 +111,24 @@ var vm = function () {
                 self.totalPages(Math.ceil(self.totalRecords() / 21));
             }
             self.SetFavourites();
-            console.log(self.records()[0].Id)
-            
+            console.log(self.records()[0].Id);
         });
     };
 
     //--- Internal functions
     function ajaxHelper(uri, method, data) {
-        self.error(''); // Clear error message
+        self.error(""); // Clear error message
         return $.ajax({
             type: method,
             url: uri,
-            dataType: 'json',
-            contentType: 'application/json',
+            dataType: "json",
+            contentType: "application/json",
             data: data ? JSON.stringify(data) : null,
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log("AJAX Call[" + uri + "] Fail...");
                 hideLoading();
                 self.error(errorThrown);
-            }
+            },
         });
     }
 
@@ -147,60 +138,57 @@ var vm = function () {
     }
 
     function showLoading() {
-        $("#myModal").modal('show', {
-            backdrop: 'static',
-            keyboard: false
+        $("#myModal").modal("show", {
+            backdrop: "static",
+            keyboard: false,
         });
     }
     function hideLoading() {
-        $('#myModal').on('shown.bs.modal', function (e) {
-            $("#myModal").modal('hide');
-        })
+        $("#myModal").on("shown.bs.modal", function (e) {
+            $("#myModal").modal("hide");
+        });
     }
 
     function getUrlParameter(sParam) {
         var sPageURL = window.location.search.substring(1),
-            sURLVariables = sPageURL.split('&'),
+            sURLVariables = sPageURL.split("&"),
             sParameterName,
             i;
         console.log("sPageURL=", sPageURL);
         for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
+            sParameterName = sURLVariables[i].split("=");
 
             if (sParameterName[0] === sParam) {
                 return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
             }
         }
-    };
+    }
 
-    self.pesquisa = function() {
+    self.pesquisa = function () {
         console.log("pesquisar...");
         self.pesquisado($("#searchbar").val().toLowerCase());
         if (self.pesquisado().length > 0) {
             window.location.href = "competitions.html?search=" + self.pesquisado();
         }
-        console.log(self.pesquisado())
-    }
+        console.log(self.pesquisado());
+    };
 
     //--- start ....
     showLoading();
-    var pg = getUrlParameter('page');
-    self.pesquisado = ko.observable(getUrlParameter('search'));
+    var pg = getUrlParameter("page");
+    self.pesquisado = ko.observable(getUrlParameter("search"));
     console.log(pg);
     if (self.pesquisado() == undefined || self.pesquisado() == "" || self.pesquisado() == null) {
-        if (pg == undefined)
-            self.activate(1);
+        if (pg == undefined) self.activate(1);
         else {
             self.activate(pg);
         }
-    }else {
+    } else {
         if (pg == undefined) self.activate2(self.pesquisado(), 1);
-        else self.activate2(self.pesquisado(), pg)
-        self.displayName = 'Found results for ' + self.pesquisado();
+        else self.activate2(self.pesquisado(), pg);
+        self.displayName = "Found results for " + self.pesquisado();
     }
     console.log("VM initialized!");
-
-    
 };
 
 $(document).ready(function () {
@@ -209,6 +197,5 @@ $(document).ready(function () {
 });
 
 $(document).ajaxComplete(function (event, xhr, options) {
-    $("#myModal").modal('hide');
-})
-
+    $("#myModal").modal("hide");
+});
